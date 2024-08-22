b08b4d3aafdb9f4b829ef126531a3280aeb17921
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-22 15:19:59 -0300

    Update Produto.js

--- src/datasets/Produto.js
+++ src/datasets/Produto.js
@@ -10,17 +10,17 @@ let produtos = [
   {
     id: '2',
     imagemUrl:
-      'https://images.unsplash.com/photo-1624128082323-beb6b8b508db?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    titulo: 'Patel de forno',
-    descricao: 'Feito com frango da Bom Todo.',
+      'https://https://media.istockphoto.com/id/1463645594/pt/foto/female-professional-groomer-combing-dog-fur-at-pet-spa-grooming-salon.jpg?s=1024x1024&w=is&k=20&c=V68wrKfh8prSsoPirb6R3VXkn7eS4K68Pl_O5f6EyRA=.unsplash.com/photo-1624128082323-beb6b8b508db?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    titulo: 'Tosa',
+    descricao: 'Deixando os pelos do seu amiguinho ainda mais belos.',
     valor: '5.0',
   },
   {
     id: '3',
     imagemUrl:
-      'https://images.unsplash.com/photo-1525385133512-2f3bdd039054?q=80&w=2570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    titulo: 'Suco de Laranja',
-    descricao: 'Feita com laranjas de colhetas tardias.',
+      'https://media.istockphoto.com/id/1707767343/pt/foto/a-young-female-groomer-brushing-a-dog-with-a-pet-brush.jpg?s=1024x1024&w=is&k=20&c=WNVPoxDhJBKpy532ApLuP1vEV5sNZgrEqd4UL3qEnNc=',
+    titulo: 'Área de secagem',
+    descricao: 'Tudo oque você precisa em um só lugar.',
     valor: '2.0',
   },
 ];
