5dc5f3237fc2076d79bb1b624daa44aac01b344f
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-22 16:09:48 -0300

    Produto.js

--- src/datasets/Produto.js
+++ src/datasets/Produto.js
@@ -2,15 +2,15 @@ let produtos = [
   {
     id: '1',
     imagemUrl:
-      'https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    titulo: 'Cookie',
-    descricao: 'Feito de cacau 100%.',
+      'https://media.istockphoto.com/id/1496991182/pt/foto/cute-cavalier-king-charles-spaniel-having-a-bath-at-home.jpg?s=1024x1024&w=is&k=20&c=QVRhy7AuUVbPJhAfTRZ4IpL-Wu-3lXwdIuoF3EoJbqw=',
+    titulo: 'Área de banho',
+    descricao: 'Sempre limpinho.',
     valor: '10.0',
   },
   {
     id: '2',
     imagemUrl:
-      'https://https://media.istockphoto.com/id/1463645594/pt/foto/female-professional-groomer-combing-dog-fur-at-pet-spa-grooming-salon.jpg?s=1024x1024&w=is&k=20&c=V68wrKfh8prSsoPirb6R3VXkn7eS4K68Pl_O5f6EyRA=.unsplash.com/photo-1624128082323-beb6b8b508db?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://media.istockphoto.com/id/1369497153/pt/foto/grooming-the-domastic-cat-in-pet-grooming-track.jpg?s=1024x1024&w=is&k=20&c=FHtpRJg8b5JbExeurh14qk-y36LtNzRdXhi9RoZBlyQ=',
     titulo: 'Tosa',
     descricao: 'Deixando os pelos do seu amiguinho ainda mais belos.',
     valor: '5.0',
