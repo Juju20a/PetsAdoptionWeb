2a6686215b459dc92ce28823ba0070d44d77f266
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-22 16:47:08 -0300

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,27 +34,24 @@ const Footer = () => {
               <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">
                   <MDBIcon color="secondary" icon="gem" className="me-3" />
-                  Salento.
+                  PetsJS.
                 </h6>
-                <p>
-                  É uma cafeteria de origem paraibana nascida na cidade de
-                  Guarabira.
-                </p>
+                <p>O melhor petshop da região.</p>
               </MDBCol>
 
               <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">Contato</h6>
                 <p>
                   <MDBIcon color="secondary" icon="home" className="me-2" />
-                  New York, NY 10012, US
+                  Paraíba, PB 10012, US
                 </p>
                 <p>
                   <MDBIcon color="secondary" icon="envelope" className="me-3" />
-                  info@example.com
+                  PetsJS@Gmail.com
                 </p>
                 <p>
                   <MDBIcon color="secondary" icon="phone" className="me-3" /> +
-                  01 234 567 88
+                  83 234 567 88
                 </p>
               </MDBCol>
             </MDBRow>
@@ -65,7 +62,7 @@ const Footer = () => {
           className="text-center p-4"
           style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
         >
-          © 2024 Salento.
+          © 2024 PetJS.
         </div>
       </MDBFooter>
     </footer>
