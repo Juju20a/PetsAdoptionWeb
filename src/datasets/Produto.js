e5941f91687418e4f3dcb38767713b9cc169126e
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-23 22:57:21 -0300

    Produto.js

--- src/datasets/Produto.js
+++ src/datasets/Produto.js
@@ -5,7 +5,7 @@ let produtos = [
       'https://media.istockphoto.com/id/1496991182/pt/foto/cute-cavalier-king-charles-spaniel-having-a-bath-at-home.jpg?s=1024x1024&w=is&k=20&c=QVRhy7AuUVbPJhAfTRZ4IpL-Wu-3lXwdIuoF3EoJbqw=',
     titulo: 'Área de banho',
     descricao: 'Sempre limpinho.',
-    valor: '10.0',
+    valor: 'Cachorro',
   },
   {
     id: '2',
@@ -13,7 +13,7 @@ let produtos = [
       'https://media.istockphoto.com/id/1369497153/pt/foto/grooming-the-domastic-cat-in-pet-grooming-track.jpg?s=1024x1024&w=is&k=20&c=FHtpRJg8b5JbExeurh14qk-y36LtNzRdXhi9RoZBlyQ=',
     titulo: 'Tosa',
     descricao: 'Deixando os pelos do seu amiguinho ainda mais belos.',
-    valor: '5.0',
+    valor: 'Gato',
   },
   {
     id: '3',
@@ -21,7 +21,7 @@ let produtos = [
       'https://media.istockphoto.com/id/1707767343/pt/foto/a-young-female-groomer-brushing-a-dog-with-a-pet-brush.jpg?s=1024x1024&w=is&k=20&c=WNVPoxDhJBKpy532ApLuP1vEV5sNZgrEqd4UL3qEnNc=',
     titulo: 'Área de secagem',
     descricao: 'Tudo oque você precisa em um só lugar.',
-    valor: '2.0',
+    valor: 'Coelho',
   },
 ];
 
