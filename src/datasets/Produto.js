fa808b899a986e926410158c90bdef20939cc7c2
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-27 14:35:42 -0300

    Produto.js

--- src/datasets/Produto.js
+++ src/datasets/Produto.js
@@ -5,7 +5,8 @@ let produtos = [
       'https://media.istockphoto.com/id/1496991182/pt/foto/cute-cavalier-king-charles-spaniel-having-a-bath-at-home.jpg?s=1024x1024&w=is&k=20&c=QVRhy7AuUVbPJhAfTRZ4IpL-Wu-3lXwdIuoF3EoJbqw=',
     titulo: 'Área de banho',
     descricao: 'Sempre limpinho.',
-    valor: 'Cachorro',
+    animaisparadoacao: 'Cachorro',
+    servicos:'Tosa'
   },
   {
     id: '2',
@@ -13,15 +14,17 @@ let produtos = [
       'https://media.istockphoto.com/id/1369497153/pt/foto/grooming-the-domastic-cat-in-pet-grooming-track.jpg?s=1024x1024&w=is&k=20&c=FHtpRJg8b5JbExeurh14qk-y36LtNzRdXhi9RoZBlyQ=',
     titulo: 'Tosa',
     descricao: 'Deixando os pelos do seu amiguinho ainda mais belos.',
-    valor: 'Gato',
+    animaisparadoacao: 'Gato',
+    servicos:'Banho'
   },
   {
     id: '3',
     imagemUrl:
-      'https://media.istockphoto.com/id/1707767343/pt/foto/a-young-female-groomer-brushing-a-dog-with-a-pet-brush.jpg?s=1024x1024&w=is&k=20&c=WNVPoxDhJBKpy532ApLuP1vEV5sNZgrEqd4UL3qEnNc=',
+      'https://media.istockphoto.com/id/1294911173/pt/foto/two-white-little-rabbits-eat-feed-from-a-plate-on-a-white-background-food-for-domestic-and.jpg?s=1024x1024&w=is&k=20&c=XYxgXWtYSpbQYblD-7dhg_kV_jmCya9tRB0UfcLH_As=',
     titulo: 'Área de secagem',
     descricao: 'Tudo oque você precisa em um só lugar.',
-    valor: 'Coelho',
+    animaisparadoacao: 'Coelho',
+    servicos:'Secagem'
   },
 ];
 
